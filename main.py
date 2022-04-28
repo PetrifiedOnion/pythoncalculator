7563446a362a31671b88257d83d03c490b3e708d
PetrifiedOnion <phillipssnicholas@gmail.com>
2022-04-28 16:58:32 -0600

    5 commit

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from re import template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Burger King!</p>"
+@app.route("/calculator")
+def index():
+    return render_template("index.html")
 
 @app.route("/<username>")
 def nick_msg(username):
