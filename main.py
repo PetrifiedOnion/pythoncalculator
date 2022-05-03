e79107e7b72fd4138b367a9d0b817339c897076a
Nicholas Phillips <phillipssnicholas@gmail.com>
2022-05-03 10:44:27 -0600

    8 commit

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from re import template
 from flask import Flask, render_template, request, flash
+from matplotlib import pyplot as plt
+import uuid
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = "static"
 
 @app.route("/calculator")
 def index():
     return render_template("index.html")
 
-@app.route("/<username>")
-def nick_msg(username):
-    return "<p>Hi %s</p>" % username
+@app.route("/calculator/make-plot")
+def make_plot():
+
+    x_values = [1, 2, 3, 4]
+    y_values = [5, 4, 6, 2]
+
+    plt.plot(x_values, y_values)
+    filename = str(uuid.uuid4())
+    plt.savefig("static/"+filename)
+    return filename
+
+@app.route("/calculator/<image>")
+def display_plot(image):
+    return render_template("index.html",plot_filename="static/%s.png" % image)
