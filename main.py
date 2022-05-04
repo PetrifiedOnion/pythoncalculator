640a7d6be30d1af3e1ec7bd615a5910e3c0c3584
Nicholas Phillips <phillipssnicholas@gmail.com>
2022-05-04 11:57:22 -0600

    11 commit... works

--- main.py
+++ main.py
@@ -1,29 +1,53 @@
 from re import template
 from flask import Flask, render_template, request, flash
-from matplotlib import pyplot as plt
+import matplotlib.pyplot  as plt
+import numpy as np
 import uuid
 import math as m
 
+# fig, ax = plt.subplots()
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "static"
 
 @app.route('/')
-@app.route("/calculator")
+@app.route("/calculator", methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+     
+        
+        datax = request.form['x_input']
+        datay = request.form['y_input']
 
-@app.route("/calculator/make-plot")
-def make_plot():
+        datax_ints = []
+        datay_ints = []
+        for i in datax.split(','):
+            datax_ints.append(int(i))
+        for i in datay.split(','):
+            datay_ints.append(int(i))
 
-    x_values = [1, 2, 3, 4]
-    y_values = [5, 4, 6, 2]
-# parse(user_input)
 
-    plt.plot(x_values, y_values)
-    filename = str(uuid.uuid4())
-    plt.savefig("static/"+filename)
-    return filename
 
-@app.route("/calculator/<image>")
-def display_plot(image):
-    return render_template("index.html",plot_filename="../static/%s.png" % image)
+
+       
+        # todo modify data so it can go into plot function below
+
+        plt.plot(datax_ints, datay_ints)
+        filename = str(uuid.uuid4())
+        plt.savefig("static/plots/"+filename)
+        return render_template("index.html",plot_filename="../static/plots/%s.png" % filename)
+
+    else:
+        return render_template("index.html", plot_filename="../static/default.jpg")
+
+
+        
+    # fig.clear(True)
+        # return render_template("index.html", plot_filename="../static/default.jpg")
+
+# @app.route("/calculator/make-plot")
+# def make_plot():
+#     return "test"
+#     # return filename
+
+# @app.route("/calculator/<image>")
+# def display_plot(image):
